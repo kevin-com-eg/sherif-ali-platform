cdf5c5bc98d5411f3a15f97a7d150f255716fd64
kevin-com-eg <k24827753@gmail.com>
2026-03-09 23:25:47 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,31 +1,42 @@
-// استيراد Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+// firebase.js
 
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth }
+from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+import { getFirestore }
+from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+import { getStorage }
+from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
 
-// إعدادات مشروعك
 const firebaseConfig = {
-  apiKey: "AIzaSyByKmG7LLj9JMq2O94gS1-da2Kt8ghLzMo",
-  authDomain: "math-e62c9.firebaseapp.com",
-  projectId: "math-e62c9",
-  storageBucket: "math-e62c9.firebasestorage.app",
-  messagingSenderId: "271507842148",
-  appId: "1:271507842148:web:2907aad4caf67afd08ab71",
-  measurementId: "G-MDJP47KKN9"
+
+apiKey: "AIzaSyByKmG7LLj9JMq2O94gS1-da2Kt8ghLzMo",
+
+authDomain: "math-e62c9.firebaseapp.com",
+
+projectId: "math-e62c9",
+
+storageBucket: "math-e62c9.appspot.com",
+
+messagingSenderId: "271507842148",
+
+appId: "1:271507842148:web:2907aad4caf67afd08ab71"
+
 };
 
 
-// تشغيل Firebase
 const app = initializeApp(firebaseConfig);
 
-
-// الخدمات
 const auth = getAuth(app);
+
 const db = getFirestore(app);
 
+const storage = getStorage(app);
 
-// تصديرهم
-export { auth, db };
+
+export { auth, db, storage };
